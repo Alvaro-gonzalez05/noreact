6224870e5208e79dca198c5954e8fd2e8b20ea3a
Gonzalez Vicario Alvaro Santino <as.gonzalez@etec.local>
2023-08-16 12:35:24 -0300

    Aprobado1

--- src/components/hedear/header.jsx
+++ src/components/hedear/header.jsx
@@ -1,10 +1,7 @@
 import './header.css';
 import carritopng from '../../multimedia/carrito.png';
 import loginpng from "../../multimedia/Login.png";
-import gift from "../../multimedia/El-sito-logo-unscreen.gif";
-import burbujaCafeteria from "../../multimedia/burbuja cafeteria.png";
-import burbujaRestobar from "../../multimedia/Burbuja restobar.png";
-import burbujaSobrenosotros from "../../multimedia/burbuja sobre nosotros.png";
+
 export default function Header() {
     return (
         <div>
@@ -25,26 +22,6 @@ export default function Header() {
         
 
         </ul>
-        
-        <div class="logogif">  
-            <img src={gift} alt="" class="gif"/>
-        </div>
-       <div class="burbujas"> 
-
-            <div className="divBurbujaTexto">
-                <img src={burbujaCafeteria} alt=""class="circulo1"/>
-                <p className="textoBurbuja1">ADSADSDASD</p>
-            </div>
-            <div>
-                <img src={burbujaRestobar} alt=""class="circulo2"/>
-                <p className="textoBurbuja2"></p>
-            </div>
-            <div>
-                <img src={burbujaSobrenosotros} alt=""class="circulo3"/>
-                <p className="textoBurbuja3"></p>
-            </div>
-            
-        </div>
         </div>
     )
 }
